bc184eaa8985ffea990860d50f5f5440b321fd77
silvio <silvio.cesar.teixeira@gmail.com>
2023-04-26 23:13:55 -0300

    Melhoria da estrutura do script 'Testar_modelo.py'

--- Testar_modelo.py
+++ Testar_modelo.py
@@ -6,15 +6,14 @@ with open('modelo_random_forest.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Carregar os dados de teste
-data_test = pd.read_excel('/home/silvio/PycharmProjects/PJI4_RandomForest/venv/Random_test.xlsx')  # Substitua pelo nome do seu arquivo de dados de teste
+data_test = pd.read_excel('venv/Realizar_Predicao.xlsx')  # Substitua pelo nome do seu arquivo de dados de teste
 
 # Separar as features (X) e o target (y)
-X_test = data_test.drop('VM_SRAG', axis=1)
-y_test = data_test['VM_SRAG']
+X_test = data_test
 
 # Fazer a predição com o modelo
 y_pred = model.predict(X_test)
 
 # Comparar as predições com os valores reais
-df_results = pd.DataFrame({'Real': y_test, 'Predito': y_pred})
+df_results = pd.DataFrame({'Predito': y_pred})
 print(df_results)
